ad6336344fc246030e5db653519d76935c543be8
RchlLou <rachelbellwood22@googlemail.com>
2022-07-20 10:49:26 -0700

    votes work in progress

--- src/Voter.jsx
+++ src/Voter.jsx
@@ -1,31 +1,33 @@
-import { patchArticleVotes } from "./api";
-import { useState, useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { UserContext } from "./UserContext"
+// import { patchArticleVotes } from "./api";
+// import { useState, useContext } from "react";
+// import { useParams } from "react-router-dom";
+// import { UserContext } from "./UserContext"
 
+// export default function Voter({ numVotes }) {
+//     const { user } = useContext(UserContext);
+//     const [votes, setVotes] = useState(0);
+//     const { article_id } = useParams();
 
-export default function Voter({ numVotes }) {
-    const { user } = useContext(UserContext);
-    const [votes, setVotes] = useState(0);
-    const { article_id } = useParams();
+//   async function patchVotes (articleId, countNum) {
+//       // let count = 0;
+//       //   count++
+//       //   if (count)
 
-    
-  console.log(numVotes);
-       
-            
-    
+//     patchArticleVotes(article_id, voteCrement).then((res) => { console.log(res)});
+//     setVotes((currentVote) => { return currentVote + 1})
 
-    return (
-        <section>
-              <p>VOTES: {numVotes + votes}</p>
-            <button onClick={() => {  patchArticleVotes(article_id, 1).then((res) => { console.log(res)}); setVotes((currentVote) => { return currentVote + 1}) }}> ♥ </button>
-        </section>
-    )
-}
+//   }
+
+//     return (
+//         <section>
+//               <p>VOTES: {numVotes + votes}</p>
+//             <button onClick={() => { patchVotes(article_id, 1) }}> ♥ </button>
+//         </section>
+//     )
+// }
 
 // function voteCrement() {
-    //   let count = 0;
-    //   count += 1;
-    //   setVote(1);
-    // }
-  
+//   let count = 0;
+//   count += 1;
+//   setVote(1);
+// }
